bcdd001d91ffb50e9d632824dd8d222a17d3dc6e
konrad.kolodziej <konradkolodziej01@gmail.com>
2021-11-28 11:11:33 +0100

    selektory

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,15 +1,42 @@
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SeleniumTest {
 
-    public static void main(String[] args) {
-        WebDriver driver = new FirefoxDriver();
+    @Test
+    public void openGooglePage() throws InterruptedException {
+        WebDriver driver = getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.get("https://www.google.com");
+
+        driver.switchTo().frame(0);
+        WebElement agreeButton = driver.findElement(By.xpath("//span[contains(test(),'Zgadzam')]"));
+        agreeButton.click();
+        driver.switchTo().defaultContent();
+        WebElement searchField = driver.findElement(By.name("q"));
+        searchField.sendKeys("Selenium");
+        searchField.sendKeys(Keys.ENTER);
+        WebElement result = driver.findElement(By.xpath("//a[contains(@href, 'selenium.dev)]//span'"));
+
+        Assert.assertTrue(result.isDisplayed());
     }
 
-    @Test
-    public void sampleTest(){
-        System.out.println("Hello!");
+    public WebDriver getDriver(String browser) {
+        switch (browser) {
+            case "chrome":
+                String chromePath = ("C:\\Users\\konrad.kolodziej\\OneDrive - IPF Digital AS\\Documents\\chromedriver\\chromedriver.exe");
+                System.setProperty("webdriver.chrome.driver", chromePath);
+                return new ChromeDriver();
+            case "firefox":
+                String firefoxPath = ("C:\\Users\\konrad.kolodziej\\OneDrive - IPF Digital AS\\Documents\\geckodriver-v0.30.0-win64\\geckodriver.exe");
+                System.setProperty("webdriver.chrome.driver", firefoxPath);
+                return new FirefoxDriver();
+            default:
+                throw  new InvalidArgumentException("Invalid browser name");
+        }
+
     }
 }
