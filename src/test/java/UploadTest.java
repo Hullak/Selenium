460922ec15b8d2ee0175b728b213623c671afbb7
konrad.kolodziej <konradkolodziej01@gmail.com>
2021-12-18 18:42:35 +0100

    getPageInfo

--- src/test/java/UploadTest.java
+++ src/test/java/UploadTest.java
@@ -2,6 +2,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
 import java.io.File;
@@ -13,10 +14,17 @@ public class UploadTest {
     public void uploadFile() throws IOException {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
+        int randomNumber = (int) (Math.random()*1000);
         driver.get("https://testeroprogramowania.github.io/selenium/fileupload.html");
-        driver.findElement(By.id("myFile")).sendKeys("C:\\Users\\konrad.kolodziej\\OneDrive - IPF Digital AS\\Documents\\plik.txt");
         TakesScreenshot screenshot = (TakesScreenshot) driver;
-        File srcFile = screenshot.getScreenshotAs(OutputType.FILE);
-        FileUtils.copyFile(srcFile,new File("src/test/resources/scrn.jpg"));
+        File before = screenshot.getScreenshotAs(OutputType.FILE);
+        String fileName = "beforeUpload" + randomNumber + ".jpg";
+        FileUtils.copyFile(before,new File("src/test/resources/" + fileName));
+        driver.findElement(By.id("myFile")).sendKeys("C:\\Users\\konrad.kolodziej\\OneDrive - IPF Digital AS\\Documents\\plik.txt");
+
+        Actions actions = new Actions(driver);
+        actions.contextClick().perform();
+        actions.contextClick(driver.findElement(By.id("myFile"))).perform();
+
     }
 }
