edd5f46cae7d7453b7075aeab997f6412f50809b
konrad.kolodziej <konradkolodziej01@gmail.com>
2021-12-22 08:40:22 +0100

    waits

--- src/test/java/ElementExistsTest.java
+++ src/test/java/ElementExistsTest.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class ElementExistsTest {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class ElementExistsTest {
+
+    WebDriver driver;
+
+    @Test
+    public void elementExistTest(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://testeroprogramowania.github.io/selenium/basics.html");
+        System.out.println(elementExist(By.tagName("p")));
+        System.out.println(elementExist(By.id("topSecret")));
+    }
+
+    public boolean elementExist(By locator) {
+
+        return driver.findElements(locator).size() > 1;
+        //        try {
+//            driver.findElement(locator);
+//            return true;
+//        } catch (NoSuchElementException ex){
+//            return  false;
+//        }
+
+    }
 }
+
--- src/test/java/WaitTest.java
+++ src/test/java/WaitTest.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class WaitTest {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+public class WaitTest {
+
+    WebDriver driver;
+
+    @Test
+    public void waitTest() throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://testeroprogramowania.github.io/selenium/wait2.html");
+
+        driver.findElement(By.id("clickOnMe")).click();
+//        Thread.sleep(5000); WebDriverWait wait = new WebDriverWait(driver, 10);
+
+        FluentWait<WebDriver> fluentWait = new FluentWait<>(driver);
+        fluentWait.ignoring(NoSuchElementException.class);
+        fluentWait.withTimeout(Duration.ofSeconds(10));
+        fluentWait.pollingEvery(Duration.ofSeconds(1));
+        fluentWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("p")));
+        driver.findElement(By.cssSelector("p"));
+    }
 }
